fd3a71f043586245fd881fff6f306a1871db3ed7
charuzain <charuabhizain@gmail.com>
2024-03-08 21:26:50 -0500

    Toggle functioanlity practice

--- 2-UseStateArray/src/App.jsx
+++ 2-UseStateArray/src/App.jsx
@@ -18,27 +18,55 @@ const data = [
 
 function App() {
   const [people, setPeople] = useState(data);
+  const [remove, setRemove] = useState(true);
+
+  const [showInfo, setShowInfo] = useState(true);
   const removeAllHandler = () => {
-    setPeople([])
-  }
+    setRemove(!remove);
+  };
 
   const deleteHandler = (id) => {
-    const filteredData = people.filter(p => p.id !== id)
-    setPeople(filteredData)
-  }
+    const filteredData = people.filter((p) => p.id !== id);
+    setPeople(filteredData);
+  };
+
+  const showHandler = () => {
+    // if (showInfo) {
+    //   setShowInfo(false);
+    // }
+    // else {
+    //   setShowInfo(true);
+    // }
+    setShowInfo(!showInfo);
+  };
 
   return (
-    <>
-      {people.map((person) => {
-        return (
-          <>
-            <h3 key={person.id}>{person.name}</h3>
-            <button onClick={()=>deleteHandler(person.id)}>Delete me</button>
-          </>
-        );
-      })}
-      <button onClick={removeAllHandler}>Remove all</button>
-    </>
+    <section>
+      {remove &&
+        people.map((person) => {
+          return (
+            <>
+              <h3 key={person.id}>{person.name}</h3>
+              <button onClick={() => deleteHandler(person.id)}>
+                Delete me
+              </button>
+            </>
+          );
+        })}
+      <button onClick={removeAllHandler}>
+        {remove ? 'Remove All' : 'show All'}
+      </button>
+      <button style={{ marginTop: '2rem' }} onClick={showHandler}>
+        Toggle
+      </button>
+      {showInfo && (
+        <div
+          style={{ backgroundColor: 'red', marginTop: '2rem', padding: '2rem' }}
+        >
+          My name is Charu
+        </div>
+      )}
+    </section>
   );
 }
 
