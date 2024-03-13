747eab51dc50460fbae33f89417a821f042411e4
charuzain <charuabhizain@gmail.com>
2024-03-13 12:40:22 -0400

    Use-reducer setup

---
+++ 6-use-reducer/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import './App.css';
+
+const data = [
+  {
+    id: 1,
+    name: 'Tim',
+  },
+  {
+    id: 2,
+    name: 'Dick',
+  },
+  {
+    id: 3,
+    name: 'Harry',
+  },
+  {
+    id: 4,
+    name: 'Peter',
+  },
+];
+function App() {
+  const [people, setPeople] = useState(data);
+  const removeHandler = (id) => {
+    const filteredNames = people.filter(person => person.id !== id)
+    setPeople(filteredNames)
+  }
+
+  const clearHandler = () => {
+    setPeople([])
+  }
+
+  const resetHandler = () => {
+    setPeople(data)
+  }
+
+  return (
+    <div className='person-container'>
+      <h1>Use Reducer</h1>
+      {people.map((person) => {
+        return (
+          <>
+            <p key={person.id}>{person.name}</p>
+            <button onClick={()=>removeHandler(person.id)}>Remove</button>
+          </>
+        );
+      })}
+      {people.length > 0 ?
+        <button onClick ={clearHandler}>Clear</button>  : <button onClick={resetHandler}>Reset</button>}
+    </div>
+  );
+}
+
+export default App;
