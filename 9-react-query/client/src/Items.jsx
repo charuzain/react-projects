2df8f04b2fee66e513ecc4b76e3c3528b25edb79
charuzain <charuabhizain@gmail.com>
2024-03-18 18:28:54 -0400

    Displayerror message

--- 9-react-query/client/src/Items.jsx
+++ 9-react-query/client/src/Items.jsx
@@ -4,20 +4,27 @@ import customFetch from './utils';
 const Items = ({ items }) => {
   const result = useQuery({
     queryKey: ['tasks'],
-    queryFn: () => customFetch.get('/'),
+    queryFn: () => customFetch.get('/s'),
   });
 
-  const { isLoading, data } = result;
+  const { isLoading, data, error, isError } = result;
   console.log(isLoading);
+  console.log(error); // axios error
+  console.log(isError); //booelan
 
+  if (isLoading) {
+    return <p>Loading....</p>;
+  }
+  if (error) {
+    // return <p>{error.message}</p>; // display axios error
+     return <p>{error.response.data}</p>; // display error message send by server
+  }
 
   return (
     <div className="items">
-      {isLoading && <p>Loading..... </p>}
-      {!isLoading &&
-        data.data.taskList.map((item) => {
-          return <SingleItem key={item.id} item={item} />;
-        })}
+      {data.data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
     </div>
   );
 };
