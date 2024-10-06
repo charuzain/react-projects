aa09e0c468d079b791c876e3b08f2bb4df37521d
charuzain <charuabhizain@gmail.com>
2024-10-06 17:53:09 -0400

    Finish Add new item functionlity

--- grocery-list/src/App.jsx
+++ grocery-list/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 function App() {
@@ -22,10 +23,32 @@ function App() {
     },
   ]);
 
+  const [newItem, setNewItem] = useState('');
+
   const deleteItem = (id) => {
     const newItems = items.filter((item) => item.id !== id);
     console.log(newItems);
     setItems(newItems);
+  };
+
+  const handleAddNewItem = (e) => {
+    console.log(e.target.value);
+    setNewItem(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(newItem);
+    if (!newItem.trim()) {
+      return;
+    }
+    const newListItem = {
+      id: items.length + 1,
+      itemName: newItem,
+      checked: false,
+    };
+    setItems([...items, newListItem]);
+    setNewItem('');
   };
 
   return (
@@ -34,8 +57,25 @@ function App() {
         <h1>Grocery List</h1>
       </header>
       <main>
+        <section>
+          <form className="addItem" onSubmit={formSubmitHandler}>
+            <input
+              type="text"
+              value={newItem}
+              onChange={(e) => handleAddNewItem(e)}
+              placeholder="Add New Item"
+              className=""
+            />
+            <button type="submit" className="submit">
+              <FontAwesomeIcon icon={faPlus} className="icon" />
+            </button>
+          </form>
+        </section>
+        <form className="search">
+          <input type="text" name="" id="" placeholder="Search for An Item" />
+        </form>
         {!items.length ? (
-          <p className='item'>No Items Present in the list</p>
+          <p className="item">No Items Present in the list</p>
         ) : (
           <ul>
             {items.map((item) => (
